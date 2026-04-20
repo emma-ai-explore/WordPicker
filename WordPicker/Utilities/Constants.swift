406915445abb6710cd3a8edb9085da44a66eabcc
chenyuling.2020 <chenyuling.2020@bytedance.com>
2026-04-20 19:40:19 +0800

    feat: add Constants configuration

---
+++ WordPicker/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+enum Constants {
+    // 窗口配置
+    static let floatWindowWidth: CGFloat = 280
+    static let floatWindowPadding: CGFloat = 12
+
+    // 查询配置
+    static let selectionDebounceInterval: TimeInterval = 0.2
+
+    // 快捷键
+    static let toggleShortcut = "cmd+shift+t"
+
+    // 词典文件名
+    static let dictFileName = "dict.db"
+}
