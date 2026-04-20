583a5c95635a46883b654eeaa55b580ab222df77
chenyuling.2020 <chenyuling.2020@bytedance.com>
2026-04-20 19:37:54 +0800

    feat: add Debouncer utility

---
+++ WordPicker/WordPicker/Services/Debouncer.swift
@@ -0,0 +1,21 @@
+import Foundation
+import Combine
+
+class Debouncer {
+    private var workItem: DispatchWorkItem?
+    private let delay: TimeInterval
+
+    init(delay: TimeInterval) {
+        self.delay = delay
+    }
+
+    func debounce(action: @escaping () -> Void) {
+        workItem?.cancel()
+        workItem = DispatchWorkItem(block: action)
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: workItem!)
+    }
+
+    func cancel() {
+        workItem?.cancel()
+    }
+}
